67688e1bd1dec668a804ca126e14159a01cb859b
HoneyGuideCN <yychcn@foxmail.com>
2017-08-14 22:22:46 +0800

    Add Palindrome Number

---
+++ LeetCode/Easy/Palindrome_Number.py
@@ -0,0 +1,11 @@
+'''
+Determine whether an integer is a palindrome.
+'''
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        return str(x) == str(x)[::-1] #so easy ^0^
