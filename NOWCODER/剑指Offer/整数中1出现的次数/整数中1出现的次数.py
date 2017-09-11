6048f29e735216e7d58d585b28d60c88b45679f9
michuanhaohao <472675005@qq.com>
2017-09-11 23:17:12 +0800

    add NumberOf1Between1AndN_Solution

---
+++ NOWCODER/剑指Offer/整数中1出现的次数/整数中1出现的次数.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 11 22:28:47 2017
+
+@author: Luohao
+"""
+
+"""
+求出1~13的整数中1出现的次数,并算出100~1300的整数中1出现的次数？
+为此他特别数了一下1~13中包含1的数字有1、10、11、12、13因此共出现6次,但是对于后面问题他就没辙了。
+ACMer希望你们帮帮他,并把问题更加普遍化,可以很快的求出任意非负整数区间中1出现的次数。
+"""
+
+
+class Solution:
+    def NumberOf1Between1AndN_Solution(self, n):
+        # write code here
+        result = 0
+        base = 1
+        roun = n
+        while roun > 0:
+            roun = roun/10
+            weight = roun%10
+            result = result + roun*base
+            if weight == 1:
+                result = result+ (n%base) + 1
+            if weight > 1:
+                result = result + base
+            base = base * 10
+        return result
+        
+                
