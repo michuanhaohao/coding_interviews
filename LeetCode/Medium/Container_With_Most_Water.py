17927bc0dc39a458a12bcb403862b16c2c62b47e
HoneyGuideCN <yychcn@foxmail.com>
2017-09-16 16:14:20 +0800

    add explanation

--- LeetCode/Medium/Container_With_Most_Water.py
+++ LeetCode/Medium/Container_With_Most_Water.py
@@ -2,6 +2,10 @@
 Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
 '''
 
+'''
+To compute area, we need to take min(height[i],height[j]) as our height. Thus if height[i]<height[j], then the expression min(height[i],height[j]) will always lead to at maximum height[i] for all other j(i being fixed), so no point checking them. Similarly when height[i]>height[j] then all the other i's can be ignored for that particular j.
+'''
+
 class Solution(object):
     def maxArea(self, height):
         """
